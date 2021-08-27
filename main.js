2714a02c2db076bd688f04f92ea522737523d46d
LeeJunKyung <away0419@naver.com>
2021-08-28 00:26:44 +0900

    분리작업1

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ app.get('/', (request, response) => {
       <h2>${title}</h2>${description}
       <img src="/images/hello.jpg" style="width:300px; display:block;">
       `,
-      `<a href="/create">create</a>`
+      `<a href="/topic/create">create</a>`
     );
     response.send(html);
 });
@@ -74,40 +74,12 @@ app.get('/', (request, response) => {
   res.send(req.params);
 }); */
 
-app.get('/page/:pageId/', (req, res, next) => {
-  fs.readdir('./data', function(error, filelist){
-    var filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf-8', function(err, description){
-      if(err){
-        next(err);// 에러 던지기 여기선 500에러
-      }else{
-        var title = req.params.pageId;
-        var sanitizedTitle = sanitizeHtml(title);
-        var sanitizedDescription = sanitizeHtml(description, {
-          allowedTags:['h1']
-        });
-        var list = template.list(filelist);
-        var html = template.HTML(sanitizedTitle, list,
-          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-          ` <a href="/create">create</a>
-            <a href="/update/${sanitizedTitle}">update</a>
-            <form action="/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-        );
-        res.send(html);
-      }
-    });
-  });
-});
-
-app.get('/create',(req,res)=>{
+app.get('/topic/create',(req,res)=>{ // 밑의 라우팅 /topic/:pageId와 겹치므로 이걸 아래보다 위에 위치시켜야함
   fs.readdir('./data',(err1,filelist)=>{
     var title = 'WEB - create';
         var list = template.list(filelist);
         var html = template.HTML(title, list, `
-          <form action="/create_process" method="post">
+          <form action="/topic/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -121,7 +93,7 @@ app.get('/create',(req,res)=>{
   });
 });
 
-app.post('/create_process', (req,res)=>{
+app.post('/topic/create_process', (req,res)=>{
   /* let body="";
   req.on('data', data=>{
     body+=data;
@@ -140,13 +112,12 @@ app.post('/create_process', (req,res)=>{
   let post = req.body;
   let title = post.title;
   let description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-    res.writeHead(302, {Location: `page/${title}`});
-    res.end();
+  fs.writeFile(`data/${title}`, description, 'utf-8', function(err){
+    res.redirect(`/topic/${title}`);
   });
 });
 
-app.get('/update/:updateId',(req,res)=>{
+app.get('/topic/update/:updateId',(req,res)=>{
   fs.readdir('./data',(error1,filelist)=>{
     let filteredId = path.parse(req.params.updateId).base;
     fs.readFile(`./data/${req.params.updateId}`,'utf-8',(error2,description)=>{
@@ -154,7 +125,7 @@ app.get('/update/:updateId',(req,res)=>{
       let list = template.list(filelist);
       let html = template.HTML(title, list,
         `
-        <form action="/update_process" method="post">
+        <form action="/topic/update_process" method="post">
           <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
           <p>
@@ -165,14 +136,14 @@ app.get('/update/:updateId',(req,res)=>{
           </p>
         </form>
         `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+        `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
       );
       res.send(html);
     });
   });
 });
 
-app.post('/update_process',(req,res)=>{
+app.post('/topic/update_process',(req,res)=>{
   /* let body = "";
   req.on('data',(data)=>{
     body+=data;
@@ -184,26 +155,57 @@ app.post('/update_process',(req,res)=>{
     var description = post.description;
     fs.rename(`data/${id}`, `data/${title}`, function(error){
       fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        res.redirect(`/page/${title}`)
+        res.redirect(`/topic/${title}`);
       })
     });
   /* }) */
 });
 
-app.post('/delete_process',(req,res)=>{
-  var body = '';
+app.post('/topic/delete_process',(req,res)=>{
+  /* var body = '';
   req.on('data', function(data){
-      body = body + data;
+    console.log(data);
+    body = body + data;
   });
-  req.on('end', function(){
-      var post = qs.parse(body);
+  req.on('end', function(){ */
+      var post = req.body;
       var id = post.id;
       var filteredId = path.parse(id).base;
       fs.unlink(`data/${filteredId}`, function(error){
         res.redirect(`/`);
       })
+  /* }); */
+});
+
+app.get('/topic/:pageId/', (req, res, next) => {
+  fs.readdir('./data', function(error, filelist){
+    var filteredId = path.parse(req.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf-8', function(err, description){
+      if(err){
+        next(err);// 에러 던지기 여기선 500에러
+      }else{
+        var title = req.params.pageId;
+        var sanitizedTitle = sanitizeHtml(title);
+        var sanitizedDescription = sanitizeHtml(description, {
+          allowedTags:['h1']
+        });
+        var list = template.list(filelist);
+        var html = template.HTML(sanitizedTitle, list,
+          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+          ` <a href="/topic/create">create</a>
+            <a href="/topic/update/${sanitizedTitle}">update</a>
+            <form action="/topic/delete_process" method="post">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
+              <input type="submit" value="delete">
+            </form>`
+        );
+        res.send(html);
+      }
+    });
   });
 });
+
+
 
 app.use((req,res,next)=>{
   res.status(404).send('sorry');
